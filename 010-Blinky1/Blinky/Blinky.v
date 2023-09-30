64c2ad811d68df339d67d9faecd281030a5c557d
jross9 <ross.ja@gmail.com>
2023-09-30 04:58:39 -0600

    Update Blinky.v

--- 010-Blinky1/Blinky/Blinky.v
+++ 010-Blinky1/Blinky/Blinky.v
@@ -1,16 +1,16 @@
 module Blinky(
-	input CLK50,
-	output LED
+	input CLK50,  // 50 MHz Clock
+	output LED    // LED to blink
 );
 
 	localparam DELAY = 25'd25_000_000-1;      // 1 blink a second  
-	// localparam DELAY = 25'd12_500_000-1;   // 
+	// localparam DELAY = 25'd12_500_000-1;   // 2 blinks a second
 
 	reg [24:0] counter;
 	reg clk;
 
 	always @(posedge CLK50) begin
-		if (counter < DELAY) // delay
+		if (counter < DELAY) 
 			counter <= counter + 1'b1;
 		else begin
 			counter <= 25'd0;
